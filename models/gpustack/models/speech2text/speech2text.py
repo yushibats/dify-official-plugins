2a8152ce75d58efcb7323ea926781e2b38bea53d
Alex Chen <nanyilikai@hotmail.com>
2025-03-16 11:05:38 +0800

    fix: gpustack speech to text schema (#465)

--- models/gpustack/models/speech2text/speech2text.py
+++ models/gpustack/models/speech2text/speech2text.py
@@ -1,6 +1,6 @@
 from typing import Optional, IO
 from dify_plugin import OAICompatSpeech2TextModel
-
+from dify_plugin.entities.model import AIModelEntity, FetchFrom, I18nObject, ModelType
 
 class GPUStackSpeechToTextModel(OAICompatSpeech2TextModel):
     """
@@ -33,3 +33,17 @@ class GPUStackSpeechToTextModel(OAICompatSpeech2TextModel):
         base_url = credentials["endpoint_url"].rstrip("/").removesuffix("/v1")
         credentials["endpoint_url"] = f"{base_url}/v1"
         return credentials
+
+    def get_customizable_model_schema(self, model: str, credentials: dict) -> Optional[AIModelEntity]:
+        """
+        Used to define customizable model schema
+        """
+        entity = AIModelEntity(
+            model=model,
+            label=I18nObject(en_US=model),
+            fetch_from=FetchFrom.CUSTOMIZABLE_MODEL,
+            model_type=ModelType.SPEECH2TEXT,
+            model_properties={},
+            parameter_rules=[],
+        )
+        return entity
