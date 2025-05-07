9cc4376a09dc94536e3324ff74fd4e808fcdd3e7
mountln <64412588+mountln@users.noreply.github.com>
2025-05-07 10:52:04 +0900

    feat: add structured output support to openai_api_compatible (#738)
    * feat: add structured output support to openai_api_compatible
    
    * style(openai_api_compatible): replace string with enum value
    
    ---------
    
    Co-authored-by: crazywoola <100913391+crazywoola@users.noreply.github.com>

--- models/openai_api_compatible/models/llm/llm.py
+++ models/openai_api_compatible/models/llm/llm.py
@@ -2,6 +2,7 @@ from typing import Mapping, Optional, Union, Generator
 
 from dify_plugin.entities.model import (
     AIModelEntity,
+    DefaultParameterName,
     I18nObject,
     ModelFeature,
     ParameterRule,
@@ -27,6 +28,35 @@ class OpenAILargeLanguageModel(OAICompatLargeLanguageModel):
                 entity.features.index(ModelFeature.AGENT_THOUGHT)
             except ValueError:
                 entity.features.append(ModelFeature.AGENT_THOUGHT)
+
+        structured_output_support = credentials.get("structured_output_support", "not_supported")
+        if structured_output_support == "supported":
+            # ----
+            # The following section should be added after the new version of `dify-plugin-sdks`
+            # is released.
+            # Related Commit:
+            # https://github.com/langgenius/dify-plugin-sdks/commit/0690573a879caf43f92494bf411f45a1835d96f6
+            # ----
+            # try:
+            #     entity.features.index(ModelFeature.STRUCTURED_OUTPUT)
+            # except ValueError:
+            #     entity.features.append(ModelFeature.STRUCTURED_OUTPUT)
+
+            entity.parameter_rules.append(ParameterRule(
+                name=DefaultParameterName.RESPONSE_FORMAT.value,
+                label=I18nObject(en_US="Response Format", zh_Hans="回复格式"),
+                help=I18nObject(
+                    en_US="Specifying the format that the model must output.",
+                    zh_Hans="指定模型必须输出的格式。",
+                ),
+                type=ParameterType.STRING,
+                options=["text", "json_object", "json_schema"],
+                required=False,
+            ))
+            entity.parameter_rules.append(ParameterRule(
+                name=DefaultParameterName.JSON_SCHEMA.value,
+                use_template=DefaultParameterName.JSON_SCHEMA.value
+            ))
 
         if "display_name" in credentials and credentials["display_name"] != "":
             entity.label = I18nObject(
