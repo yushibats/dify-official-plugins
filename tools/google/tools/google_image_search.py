22b528099107a3d7c103952cef21c26dc86e3818
luckylhb90 <luckylhb90@gmail.com>
2025-05-12 04:47:34 +0300

    feat:support google image search (#893)
    * feat:support google image search
    
    * feat:support google image search
    
    ---------
    
    Co-authored-by: hobo.l <hobo.l@binance.com>

--- tools/google/tools/google_image_search.py
+++ tools/google/tools/google_image_search.py
@@ -1,16 +1,11 @@
 import json
-
-from collections.abc import Generator
-from typing import Any
+import os
+from itertools import islice
+from typing import Any, Generator
 
 import requests
-
-from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
-
-import os
-
-SERP_API_URL = "https://serpapi.com/search"
+from dify_plugin import Tool
 
 
 def get_file_path(filename: str) -> str:
@@ -31,32 +26,36 @@ def load_valid_languages(filename: str) -> set:
         return {language['language_code'] for language in languages}
 
 
+SERP_API_URL = "https://serpapi.com/search"
 VALID_COUNTRIES = load_valid_countries(get_file_path("google-countries.json"))
 VALID_LANGUAGES = load_valid_languages(get_file_path("google-languages.json"))
 
 
-class GoogleSearchTool(Tool):
-    def _parse_response(self, response: dict) -> dict:
+class GoogleImageSearchTool(Tool):
+    def _parse_response(self, response: dict, max_results: int) -> dict:
         result = {}
-        if "knowledge_graph" in response:
-            result["title"] = response["knowledge_graph"].get("title", "")
-            result["description"] = response["knowledge_graph"].get("description", "")
-        if "organic_results" in response:
-            result["organic_results"] = [
+        if "images_results" in response:
+            result["images"] = [
                 {
                     "title": item.get("title", ""),
-                    "link": item.get("link", ""),
-                    "snippet": item.get("snippet", ""),
+                    "image": item.get("original", ""),
+                    "thumbnail": item.get("thumbnail", ""),
+                    "url": item.get("link", ""),
+                    "height": item.get("original_height", ""),
+                    "width": item.get("original_width", ""),
+                    "source": item.get("source", ""),
                 }
-                for item in response["organic_results"]
+                for item in islice(response["images_results"], max_results)
             ]
         return result
 
-    def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
+    def _invoke(
+            self, tool_parameters: dict[str, Any]
+    ) -> Generator[ToolInvokeMessage, None, None]:
         hl = tool_parameters.get("hl", "en")
         gl = tool_parameters.get("gl", "us")
         location = tool_parameters.get("location", "")
-        imgsz = tool_parameters.get("imgsz", "m")
+        max_results = tool_parameters.get("max_results", 3)
 
         # Validate 'hl' (language) code
         if hl not in VALID_LANGUAGES:
@@ -70,20 +69,24 @@ class GoogleSearchTool(Tool):
 
         params = {
             "api_key": self.runtime.credentials["serpapi_api_key"],
-            "q": tool_parameters["query"],
-            "engine": "google",
-            "google_domain": "google.com",
+            "q": tool_parameters['query'],
+            "engine": "google_images",
             "gl": gl,
             "hl": hl,
-            "imgsz": imgsz
+            "location": location
         }
-        if location:
-            params["location"] = location
         try:
             response = requests.get(url=SERP_API_URL, params=params)
             response.raise_for_status()
-            valuable_res = self._parse_response(response.json())
-            yield self.create_json_message(valuable_res)
+
+            valuable_res = self._parse_response(response.json(), max_results)
+            markdown_result = "\n\n"
+            json_result = []
+            for res in valuable_res.get("images", []):
+                res['extension'] = f".{res['image'].split('.')[-1]}"
+                markdown_result += f"![{res.get('title') or ''}]({res.get('image') or ''})"
+                json_result.append(self.create_json_message(res))
+            yield from [self.create_text_message(markdown_result)] + json_result
         except requests.exceptions.RequestException as e:
             yield self.create_text_message(
                 f"An error occurred while invoking the tool: {str(e)}. Please refer to https://serpapi.com/locations-api for the list of valid locations.")
