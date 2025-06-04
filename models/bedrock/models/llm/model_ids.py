970523ffd31b470ae88ce36f3020105b120b4038
Warren Chen <warren.chen830@gmail.com>
2025-06-04 19:34:02 +0800

    add nova premier (#1030)
    * add nova premier
    
    * fix

--- models/bedrock/models/llm/model_ids.py
+++ models/bedrock/models/llm/model_ids.py
@@ -21,8 +21,9 @@ BEDROCK_MODEL_IDS = {
     'amazon nova': {
         'Nova Pro': 'amazon.nova-pro-v1:0',
         'Nova Lite': 'amazon.nova-lite-v1:0',
-        'Nova Micro': 'amazon.nova-micro-v1:0'
-    },
+        'Nova Micro': 'amazon.nova-micro-v1:0',
+        'Nova Premier': 'amazon.nova-premier-v1:0'
+},
     'meta': {
         'Llama 3 8B Instruct': 'meta.llama3-8b-instruct-v1:0',
         'Llama 3 70B Instruct': 'meta.llama3-70b-instruct-v1:0',
