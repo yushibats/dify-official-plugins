006578e837b62b121926cad91cd15ab3b1c42a1c
Bowen Liang <liang.bowen.123@qq.com>
2025-06-04 19:37:05 +0800

    update (#1021)

--- tools/qrcode/tools/qrcode_generator.py
+++ tools/qrcode/tools/qrcode_generator.py
@@ -1,12 +1,16 @@
 import io
 import logging
-from typing import Any, Generator
+import os
+import tempfile
+from typing import Any, Generator, Optional
 
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
+from dify_plugin.file.file import File
 from qrcode.constants import ERROR_CORRECT_H, ERROR_CORRECT_L, ERROR_CORRECT_M, ERROR_CORRECT_Q
 from qrcode.image.base import BaseImage
 from qrcode.image.pure import PyPNGImage
+from qrcode.image.styledpil import StyledPilImage
 from qrcode.main import QRCode
 
 
@@ -19,7 +23,7 @@ class QRCodeGeneratorTool(Tool):
     }
 
     def _invoke(
-        self, tool_parameters: dict[str, Any]
+            self, tool_parameters: dict[str, Any]
     ) -> Generator[ToolInvokeMessage, None, None]:
         """
         invoke tools
@@ -33,23 +37,60 @@ class QRCodeGeneratorTool(Tool):
         error_correction = tool_parameters.get("error_correction", "")
         if error_correction not in self.error_correction_levels:
             yield self.create_text_message("Invalid parameter error_correction")
+
+        embedded_image: Optional[File] = tool_parameters.get("embedded_image")
+        embedded_image_path: Optional[str] = None
+        if embedded_image:
+            tmp_embedded_file = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+            tmp_embedded_file.write(embedded_image.blob)
+            tmp_embedded_file.flush()
+            embedded_image_path = tmp_embedded_file.name
+
+        output_filename: Optional[str] = tool_parameters.get("output_filename")
+        if output_filename and not output_filename.lower().endswith(".png"):
+            final_output_filename = f"output_filename.png"
+        else:
+            final_output_filename = output_filename
+
         try:
-            image = self._generate_qrcode(content, border, error_correction)
+            image = self._generate_qrcode(content, border, error_correction, embedded_image_path=embedded_image_path)
             image_bytes = self._image_to_byte_array(image)
             yield self.create_blob_message(
-                blob=image_bytes, meta={"mime_type": "image/png"}
+                blob=image_bytes,
+                meta={
+                    "mime_type": "image/png",
+                    "filename": final_output_filename,
+                },
             )
         except Exception:
             logging.exception(f"Failed to generate QR code for content: {content}")
             yield self.create_text_message("Failed to generate QR code")
+        finally:
+            if embedded_image_path:
+                os.unlink(embedded_image_path)
+            pass
 
-    def _generate_qrcode(self, content: str, border: int, error_correction: str) -> BaseImage:
+    def _generate_qrcode(self, content: str, border: int, error_correction: str,
+                         embedded_image_path: Optional[str]) -> BaseImage:
+        if embedded_image_path:
+            image_factory = StyledPilImage
+            error_correction = "H"
+            embedded_image_ratio = 0.3
+        else:
+            image_factory = PyPNGImage
+            embedded_image_ratio = None
+
         qr = QRCode(
-            image_factory=PyPNGImage, error_correction=self.error_correction_levels.get(error_correction), border=border
+            image_factory=image_factory,
+            error_correction=self.error_correction_levels.get(error_correction),
+            border=border
         )
         qr.add_data(data=content)
         qr.make(fit=True)
-        img = qr.make_image()
+        img = qr.make_image(
+            embedded_image_path=embedded_image_path,
+            embedded_image_ratio=embedded_image_ratio,
+        )
         return img
 
     @staticmethod
