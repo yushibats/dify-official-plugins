1e3cfe0ee099ac37e970f71a622b0084d5360421
rhq-git <54261696+rhq-git@users.noreply.github.com>
2025-06-20 09:25:31 +0800

    add international endpoint for llm (#1102)
    * add international endpoint for llm
    
    * fix: update invalid endpoint params and bump api version to 0.0.27
    
    * Change the component type to radio button
    
    * fix label data type
    
    ---------
    
    Co-authored-by: rhq <rhq02295491@alibaba-inc.com>

--- models/tongyi/models/llm/llm.py
+++ models/tongyi/models/llm/llm.py
@@ -171,6 +171,9 @@ class TongyiLargeLanguageModel(LargeLanguageModel):
         :param user: unique user id
         :return: full response or stream response chunk generator result
         """
+        if credentials.get("use_international_endpoint", "false") is "true":
+            import dashscope
+            dashscope.base_http_api_url = "https://dashscope-intl.aliyuncs.com/api/v1"
         credentials_kwargs = self._to_credential_kwargs(credentials)
         mode = self.get_model_mode(model, credentials)
         if model in {"qwen-turbo-chat", "qwen-plus-chat"}:
